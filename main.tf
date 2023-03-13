bf3494ac4d67686d8ae42c7d11d5ae9f34ffeff4
dgajbhiye <123443328+dgajbhiye@users.noreply.github.com>
2023-03-13 14:35:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region  = var.aws_region
 
-  # You can use access keys
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
   # Or specify an aws profile, instead.
-  # profile = "<aws profile>"
+  profile = "<aws profile>"
 }
 
 data "aws_ami" "ubuntu" {
